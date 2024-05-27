df2ef72e83a34f48f1e8446692d54b0120ec9cc8
Moavia Mahmood <168455506+MoaviaMahmood@users.noreply.github.com>
2024-05-27 14:40:53 +0300

    Delete most common pize orderd.sql

--- most common pize orderd.sql
+++
@@ -1,8 +0,0 @@
--- most common pizza pize orderd
-
-select pizzas.size, count(orders_details.order_details_id) AS orders_count
-from pizzas
-join orders_details
-on pizzas.pizza_id = orders_details.pizza_id
-group by pizzas.size
-order by orders_count desc;

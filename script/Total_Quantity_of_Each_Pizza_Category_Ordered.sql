7ea0ee31fad7cd9b23c67ff51843a14ab1dd7f8e
Moavia Mahmood <168455506+MoaviaMahmood@users.noreply.github.com>
2025-08-02 14:53:32 +0300

    Create Total_Quantity_of_Each_Pizza_Category_Ordered.sql

---
+++ script/Total_Quantity_of_Each_Pizza_Category_Ordered.sql
@@ -0,0 +1,10 @@
+  SELECT 
+      pizza_types.category,
+      SUM(orders_details.quantity) AS quantity
+  FROM
+      pizza_types
+          JOIN
+      pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+          JOIN
+      orders_details ON orders_details.pizza_id = pizzas.pizza_id
+  GROUP BY pizza_types.category
